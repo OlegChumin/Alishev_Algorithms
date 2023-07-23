8fe65d7506c6d130f7e922b964cc2cdb2a38fe0d
Oleg Chumin <tschumin.oleg@gmail.com>
2023-07-23 09:46:27 +0300

    CRUDE code

--- src/main/java/lesson_01/Fibonacci.java
+++ src/main/java/lesson_01/Fibonacci.java
@@ -1,4 +1,18 @@
 package lesson_01;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+
+    }
+
+    //наивный медленный алгоритм
+    private static long fibNative(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1L;
+        } else {
+            return 0L;
+        }
+    }
 }
