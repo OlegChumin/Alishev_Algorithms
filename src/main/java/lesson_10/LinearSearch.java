35f1392d0ec624506f8305c6a106c9941d24bb16
Oleg Chumin <tschumin.oleg@gmail.com>
2023-07-25 14:16:27 +0300

    CRUDE code

--- src/main/java/lesson_10/LinearSearch.java
+++ src/main/java/lesson_10/LinearSearch.java
@@ -1,20 +1,26 @@
 package lesson_10;
 
+import org.omg.IOP.ENCODING_CDR_ENCAPS;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Random;
 
+import static java.lang.Math.random;
+
 public class LinearSearch {
-    private static final Random random = new Random();
+    private static Random random = new Random();
+    private static final int START_RANGE_OF_RANDOM = 0;
+    private static final int END_RANGE_OF_RANDOM = 1_000_000;
 
     public static void main(String[] args) {
-        int[] array = generateIntRandomArray(getLengthOfArrayToGenerate());
-
+        int[] array = generateAndFillIntRandomArray(getLengthOfArrayToBeGenerated());
     }
 
-    private static int getLengthOfArrayToGenerate() {
+    private static int getLengthOfArrayToBeGenerated() {
         System.out.println("Pls. enter length of array to be generated: ");
         try {
             return Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
@@ -23,8 +29,10 @@ public class LinearSearch {
         }
     }
 
-    private static int[] generateIntRandomArray(int nLength) {
-        return new int[nLength];
+    private static int[] generateAndFillIntRandomArray(int nLength) {
+        int[] array = new int[nLength];
+        Arrays.fill(array, random.nextInt(101));
+        return array;
     }
 
 }
